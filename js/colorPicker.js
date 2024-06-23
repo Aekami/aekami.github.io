8d4bf38edaec2e2768fe6c611d0a8ea71f180202
Aekami <Aekami@users.noreply.github.com>
2024-06-23 14:22:32 +0200

    -

--- js/colorPicker.js
+++ js/colorPicker.js
@@ -1,5 +1,5 @@
 
-import { tilesInfos } from "../json/tileInfos.js";
+import tilesInfos from "../Json/tileInfos.js";
 
     function copyToClipboard(textToCopy = "Copied text was empty."){
         navigator.clipboard.writeText(textToCopy); 
