ca01fcbb04a0367d38c3fc9fe533c689df333253
Aekami <Aekami@users.noreply.github.com>
2024-06-26 03:24:13 +0200

    - added js to dynamically load navBars - added two different navbars : |-- the one for the home page (which will have the different generic games/subjects (ex: Wakfu, League, CyberSecu, etc.)) |-- the one to be used for all pages under /Wakfu

---
+++ js/navBarLoader.js
@@ -0,0 +1,17 @@
+
+function loadNavBar() {
+    fetch('../navBar.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error fetching navBar.');
+            }
+            return response.text();
+        })
+        .then(data => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data, 'text/html');
+            const navbar = doc.querySelector('navBar');
+            document.getElementById('nav').appendChild(navbar);
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+}
