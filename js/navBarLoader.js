596be2abbb937c85d363d4bfc66b34a8fca6bdbf
Aekami <Aekami@users.noreply.github.com>
2024-06-26 06:36:27 +0200

    -

--- js/navBarLoader.js
+++ js/navBarLoader.js
@@ -15,8 +15,35 @@ function loadNavBar() {
             const doc = parser.parseFromString(data, 'text/html');
             const navbar = doc.querySelector('.navBar');
             document.querySelector('.nav').appendChild(navbar);
+
+            if (currentPath.includes('/wakfu/')) {
+                pathToNavBar = baseURL + '/navBars/wakfu/navBar.html';
+            } else {
+                pathToNavBar = baseURL + '/navBars/home/navBar.html';
+                loadWakfuLinks();
+            }
+
+
         })
         .catch(error => console.error('Error loading navbar:', error));
 }
-    // document.querySelector(".nav");
-    // let fragment = document.createDocumentFragment();
+
+// TODO refactor, should be oblivious to whatver the actual html is. Move to the html navBar file itself.
+function loadWakfuLinks(){
+
+    const homeLink = document.getElementById('homeLink');
+    homeLink.href = `${window.location.origin}/index.html`;
+
+    const imageLink = document.getElementById('imageLink');
+    imageLink.href = `${window.location.origin}/images/aekami_elio.png`;
+
+    const colorPicker = document.getElementById('colorPickerLink');
+    colorPicker.href = `${window.location.origin}/pages/colorPicker.html`;
+
+    const wakfuHomeLink = document.getElementById('wakfuHomeLink');
+    wakfuHomeLink.href = `${window.location.origin}/wakfu/wakfu-home.html`;
+
+    const aboutLink = document.getElementById('aboutLink');
+    aboutLink.href = `${window.location.origin}/pages/about.html`;
+}
+    
