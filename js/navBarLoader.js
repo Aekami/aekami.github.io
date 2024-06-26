39b77b66987cc4120222360f68d8e4089f16eb83
Aekami <Aekami@users.noreply.github.com>
2024-06-26 03:28:48 +0200

    - modified navBarloader

--- js/navBarLoader.js
+++ js/navBarLoader.js
@@ -11,7 +11,9 @@ function loadNavBar() {
             const parser = new DOMParser();
             const doc = parser.parseFromString(data, 'text/html');
             const navbar = doc.querySelector('navBar');
-            document.getElementById('nav').appendChild(navbar);
+            document.getElementById('.nav').appendChild(navbar);
         })
         .catch(error => console.error('Error loading navbar:', error));
 }
+    // document.querySelector(".nav");
+    // let fragment = document.createDocumentFragment();
